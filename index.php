d8768b116e155abce10ab0b2ed02a7df7c1938cd
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-03 15:07:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./server.php">
+        <div class="msg">
+           <label for="message">Messaggio</label>
+           <textarea name="message" id="message"></textarea>
+        </div>
+        <div class="word">
+            <label for="word">Word</label>
+            <input type="text" id="word" name="word">
+        </div>
+        <button>Sent</button>
+    </form>
+</body>
+</html>
+
---
+++ server.php
@@ -0,0 +1,7 @@
+<pre>
+    <?php
+        // var_dump($_GET);
+        $message = $_GET['message'];
+        $word = $_GET['word'];
+        var_dump($message);
+        var_dump($word);
